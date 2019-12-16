62ac029ec9841faef787fedd18907d5444028f11
Firas1993 <kebsi.firas@competitoor.com>
2019-12-16 21:47:46 +0100

    set up the component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 
 import "./App.css";
 
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Room from "./pages/Room";
+import Rooms from "./pages/Rooms";
+
 
 function App() {
   return (
     <div className="">
      Hello From APP
+     <hr />
+     <Home />
+     <Rooms />
+     <Room />
+     <Error />
+
     </div>
   );
 }
